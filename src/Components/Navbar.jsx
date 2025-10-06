993607d6307178f762dcabf63070ca4a85441207
Piyush1532 <piyushadav503@gmail.com>
2025-10-06 15:24:05 +0530

    added backdrop for sidebar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,12 +8,20 @@ const [sideBarOpen,setSideBarOpen]=useState(false)
 
 
   return (
-<div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-24 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
+<div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-24 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70' >
 
       <img src={theme === 'dark' ? assets.logo_dark : assets.logo}   alt="logo" className='w-12 sm:w-40' />
 
 
-      <div className={`text-gray-700 dark:text-white sm:text-sm ${!sideBarOpen ? 'max-sm:w-0 overflow-hidden' : 'max-sm:w-60 max-sm:pl-10'} max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-[#6200de] max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5   transition-all duration-300`}>
+
+{sideBarOpen && (
+  <div
+    className='fixed top-0 left-0 w-full h-screen bg-black/40 z-1 sm:hidden'
+    onClick={() => setSideBarOpen(false)}
+  />
+)}
+
+      <div className={`text-gray-700 dark:text-white sm:text-sm ${!sideBarOpen ? 'max-sm:w-0 overflow-hidden' : 'max-sm:w-60 max-sm:pl-10'} max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-[#6200de] max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5   transition-all duration-300 z-10`}>
         <img src={assets.close_icon} alt="" className='w-5 absolute right-4 top-4 sm:hidden' onClick={()=>setSideBarOpen(false)}/>
    
         <a href="#" className='sm:hover:border-b ' onClick={()=>setSideBarOpen(false)}>Home</a>
