d9b98ec21ccf3fb523c4f660e7db62f1799543c5
Piyush1532 <piyushadav503@gmail.com>
2025-10-06 15:23:37 +0530

    mounted components and toaster

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from './Components/Navbar'
 import Hero from './Components/Hero';
 import Trustedby from './Components/Trustedby';
 import Services from './Components/Services';
+import Work from './Components/Work';
+import Teams from './Components/Teams';
+import Contactus from './Components/Contactus';
+import {Toaster} from "react-hot-toast"
 
 
 const App = () => {
@@ -24,11 +28,14 @@ const App = () => {
 
   return (
 <div className=' dark:bg-gray-900/100 transition-colors duration-300 relative min-h-screen'> 
+<Toaster/>
   <Navbar theme={theme} setTheme={setTheme} /> 
   <Hero/>
   <Trustedby/>
 <Services/>
-
+  <Work/>
+  <Teams/>
+<Contactus/>
 </div>
 
   )
