983992661392315771aa2aa88adbf5001997a842
Piyush1532 <piyushadav503@gmail.com>
2025-10-08 15:14:29 +0530

    mounted footer and created custom pointer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero';
 import Trustedby from './Components/Trustedby';
@@ -7,15 +7,50 @@ import Work from './Components/Work';
 import Teams from './Components/Teams';
 import Contactus from './Components/Contactus';
 import {Toaster} from "react-hot-toast"
+import Footer from './Components/Footer';
 
 
 const App = () => {
 
-// const [theme,setTheme]=useState(localStorage.getItem("theme") ? localStorage.getItem("theme") :"light" )
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
 
+
+  const dotRef = useRef(null);
+  const outlineRef = useRef(null);
+
+  const customPointer = useRef({ x: 0, y: 0 });
+  const position = useRef({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (evt) => {
+      customPointer.current.x = evt.clientX;
+      customPointer.current.y = evt.clientY;
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    const animate = () => {
+      position.current.x += (customPointer.current.x - position.current.x) * 0.1;
+      position.current.y += (customPointer.current.y - position.current.y) * 0.1;
+
+      if (dotRef.current && outlineRef.current) {
+        dotRef.current.style.transform = `translate3d(${position.current.x - 6}px, ${position.current.y -  6}px, 0)`;
+        outlineRef.current.style.transform = `translate3d(${position.current.x -20}px, ${position.current.y -20}px, 0)`;
+      }
+
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  
   useEffect(() => {
   if (theme === "dark") {
     document.documentElement.classList.add("dark");
@@ -36,7 +71,24 @@ const App = () => {
   <Work/>
   <Teams/>
 <Contactus/>
+<Footer theme={theme}/>
+
+
+{/* custom cursor div */}
+   <div
+        ref={outlineRef}
+        className="fixed top-0 left-0 h-10 w-10 rounded-full border border-[#6200de] pointer-events-none z-[9999]"
+
+      />
+      <div
+        ref={dotRef}
+        className='fixed top-0 left-0 h-3 w-3 rounded-full bg-[#6200de] pointer-events-none z-[9999]'
+
+      />
+
 </div>
+
+
 
   )
 }
